797869fa68fd42d282f2568e78b1ed55e5005c8d
Milan JUNGES <junges.milan22@gmail.com>
2023-11-17 15:06:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const elements = document.querySelectorAll(".element");
+// variable qui stock l'id de l'élément sélectionner (terre, soleil...)
+let currentElement = "";
+
+// On ajoute un événement sur chacun des boutons des éléments du système solaire qui se déclenche quand on clique dessus
+elements.forEach((element) => {
+    element.addEventListener("click", () => {
+        // currentElement prend la valeur de l'id de l'élément sur lequel on a cliqué
+        currentElement = element.id;
+        while (document.body.firstChild) {
+            // on retire tous les éléments de la page
+            document.body.removeChild(document.body.firstChild);
+        }
+        // on appelle la fonction qui sert à afficher la page d'info sur l'élément cliqué
+        displayElementData(currentElement);
+    });
+});
+
+function displayElementData(id) {
+    // on fait la requête API qui sert à récupérer les données sur l'élément cliqué
+    fetch(`https://api.le-systeme-solaire.net/rest.php/bodies/${id}`)
+        // on met le résultat sous le format json supporté par JavaScript pour pouvoir manipuler la data venant de l'API
+        .then((res) => res.json())
+        // on utilise les données récupérées pour les afficher de la manière qu'on veut
+        .then((data) => {
+            // vérif (à supp)
+            console.log(data);
+            // créer une liste des propriétés (<ul> -> <li> / <ol> -> <li>)
+            for (var key in data) {
+                // on vérifie que la propriété est bien celle de l'objet json et ne provient pas d'un héritage
+                if (data.hasOwnProperty(key)) {
+                    let p = document.createElement("p");
+                    p.textContent = `${key} : ${data[key]}`;
+                    document.body.appendChild(p);
+                }
+            }
+        })
+        // si une erreur survient lors de la création de la nouvelle page d'affichage (après le dernier "then"),
+        // l'erreur s'affiche dans la console.
+        .catch((err) => {
+            console.log(err);
+        });
+}
